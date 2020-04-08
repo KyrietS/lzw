2c86756f645b99802d6d08c6f9609f790bcb060f
Kyriet <kyriet.sebastian@gmail.com>
2020-04-09 01:42:44 +0200

    Added progress bar when -s,--stats option is active

--- LZW_Core/src/LZWCoder.hpp
+++ LZW_Core/src/LZWCoder.hpp
@@ -6,6 +6,14 @@
 class LZWCoder : public DictionaryCoder
 {
 public:
+	LZWCoder(bool printProgress = false)
+		: printProgress(printProgress) {}
+
 	Statistics encode(const std::string & path_in, const std::string & path_out) override;
 	void decode(const std::string & path_in, const std::string & path_out) override;
+private:
+	bool printProgress;
+	int currentProgress = 0;
+
+	inline void updateProgress(double progress);
 };
